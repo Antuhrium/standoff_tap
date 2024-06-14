43b6c0fd24ae11e892559e830700b4ec20924e7b
Antuhrium <antyriyman@gmail.com>
2024-06-14 03:21:31 +0300

    xcbcx

--- src/pages/RatePage.tsx
+++ src/pages/RatePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { IFriendsArray, friendsArray } from "../utils/data";
 import { useNavigate } from "react-router-dom";
 
